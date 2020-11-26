01f040c1efae7ed36750fbabcde6020b17ea1726
Edit Kovacs <edit.k86@gmail.com>
2020-11-26 15:07:25 +0100

    exeptionhandling w Patient 1/2;

---
+++ JavaBasic/src/main/java/exceptionhandling/Patient.java
@@ -0,0 +1,32 @@
+package exceptionhandling;
+
+public class Patient {
+
+    private String name;
+    private String socialSecurityNumber;
+    private int yearOfBirth;
+   // private SsnValidator ssnValidator = new SsnValidator();
+
+    public Patient(String name, String ssn, int yearOfBirth) {
+        if (name.isEmpty() && yearOfBirth < 1900 && ! new SsnValidator().isValidSsn(ssn)){
+            throw new IllegalArgumentException("Helytelen adatok: \nNév: " +name
+                    +"\nSzületési év: "+yearOfBirth
+                    +"\nTAJ: "+ssn);
+        }
+        this.name = name;
+        this.socialSecurityNumber = ssn;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+}
---
+++ JavaBasic/src/main/java/exceptionhandling/SsnValidator.java
@@ -0,0 +1,50 @@
+package exceptionhandling;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class SsnValidator {
+
+    public boolean isValidSsn(String ssn){
+        try {
+            //int value = Integer.parseInt(ssn);
+            char[] ch = ssn.toCharArray();
+            int[] ints = new int[9];
+            for (int i=0; i < ch.length; i++) {
+                ints[i] = Integer.parseInt(String.valueOf(ch[i]));
+            }
+            int paratlan = 0;
+            int paros = 0;
+            for (int i = 0; i < ints.length-1; i++) {
+                if ((i+1)%2 ==1){
+                   paratlan+= ints[i]*3;
+                }
+                else{
+                    paros += ints[i]*7;
+                }
+            }
+            int numNine = (paros+paratlan)%10;
+
+            return numNine==ints[ints.length-1];
+
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+
+   public int ParseSsn(String ssn) {
+      return   Integer.parseInt(ssn);
+    }// We now know that it's safe to parse
+
+    boolean tryParseInt(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
